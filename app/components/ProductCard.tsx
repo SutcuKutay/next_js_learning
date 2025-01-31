e99929cb3f46227cb2cd45f8630b223c28835927
Kutay Sütçü <94748829+SutcuKutay@users.noreply.github.com>
2025-01-31 17:47:16 +0300

    day 1

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AddToCart from "./AddToCart";
+
+const ProductCard = () => {
+  return (
+    <div>
+      <h2>This is a product card</h2>
+      <AddToCart></AddToCart>
+    </div>
+  );
+};
+
+export default ProductCard;
